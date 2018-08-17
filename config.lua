4e7b3a59cb56aaa182d08e1dc1490335974a71e7
aaculovich <cat@ater.me>
2018-08-18 00:35:34 +0300

    change resolution

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ gameBuildVersion = "2018.08.17"
 application = {
     content = {
         width = appScale * 900,
-        height = appScale * 1440,
+        height = appScale * 1600,
         scale = 'letterbox',
         fps = 60,
     }
